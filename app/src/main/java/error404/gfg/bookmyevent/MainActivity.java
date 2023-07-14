33f3343a2d582372193699d8a107e9cedd2ab4a6
The Expensive Informatics <107309589+theexpensiveinformatics@users.noreply.github.com>
2023-07-14 19:13:38 +0530

    Initial commit

---
+++ app/src/main/java/error404/gfg/bookmyevent/MainActivity.java
@@ -0,0 +1,14 @@
+    package error404.gfg.bookmyevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
